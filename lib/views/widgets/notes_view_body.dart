4063ce68636c4b4add894250f4bc4df8c330e82a
abd brk <abdbrkhsh@gmail.com>
2023-03-23 15:37:53 +0200

    create notes list view

--- lib/views/widgets/notes_view_body.dart
+++ lib/views/widgets/notes_view_body.dart
@@ -1,65 +1,42 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
 
 import 'package:notes_app/views/widgets/custom_app_bar.dart';
+
+import 'custom_note_item.dart';
 
 class NotesViewBody extends StatelessWidget {
   const NotesViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return  Padding(
+    return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 24),
       child: Column(
-          children:const [
-            SizedBox(
-              height: 60,
-            ),
-            CustomAppBar(),
-            NoteItem()
-          ],
+        children: const [
+          SizedBox(
+            height: 60,
+          ),
+          CustomAppBar(),
+          Expanded(child: NotesListView())
+        ],
       ),
     );
   }
 }
 
 
-class NoteItem extends StatelessWidget {
-  const NoteItem({super.key});
+class NotesListView extends StatelessWidget {
+  const NotesListView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return  Container(
-      padding: EdgeInsets.only(top: 24,bottom: 24,left: 16),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(16),
-        color: const Color(0xffFFCC80)
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children:  [
-          ListTile(
-              title: Text('Flutter Tips' ,style: TextStyle(
-                color: Colors.black,fontSize: 26
-              ),),
-              subtitle: Padding(
-                padding: const EdgeInsets.only(top: 16,bottom: 16),
-                child: Text('Build your carerr with tharwat samy ',style: TextStyle(
-                  color: Colors.black.withOpacity(0.5),fontSize: 18
-                ),),
-              ),
-              trailing: IconButton(onPressed: (){}, icon: Icon(FontAwesomeIcons.trash,color: Colors.black,size: 24,) ,),
-          ),
-
-          Padding(
-            padding: const EdgeInsets.only(right: 16 ),
-            child: Text('May 22 ,2022',style: TextStyle(
-                  color: Colors.black.withOpacity(0.5),fontSize: 14
-                ),),
-          )
-        ],
-      ),
-    ) ;
+    return ListView.builder(itemBuilder: (context,index){
+      return Padding(
+        padding: const EdgeInsets.symmetric(vertical: 8),
+        child: NoteItem(),
+      );
+    });
   }
 }
